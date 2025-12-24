c88abd3daf3340e645b5ad292de4d354f53b1eda
Flaviu Tamas <me@flaviutamas.com>
2025-12-23 20:10:45 -0500

    cci config

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,17 +21,29 @@ build () {
 	echo "* $(date): building $NAME (using JOBS=$JOBS)" | tee -a "$LOG"
 	echo "* $(date): log in $LOG" | tee -a "$LOG"
 	echo "*" | tee -a "$LOG"
-	time DOCKER_BUILDKIT=1 BUILDKIT_PROGRESS=plain docker buildx build \
-		"$DIR" \
-		"$@" \
-		-t "$TAG":latest \
-		--progress=plain \
-		--build-arg="JOBS=$JOBS" \
-		-f "$DIR"/"$NAME"/Dockerfile \
-		--output=type=oci,tar=false,dest="${OCI}" \
-		--output=type=oci,tar=true,dest="${OCI}.tar" \
-		2>&1 | tee -a "$LOG"
-	time docker load -i "${OCI}.tar" 2>&1 | tee -a "$LOG"
+	if [ -n "${CIRCLECI:-}" ] || [ -n "${CI:-}" ]; then
+		time DOCKER_BUILDKIT=1 BUILDKIT_PROGRESS=plain docker buildx build \
+			"$DIR" \
+			"$@" \
+			-t "$TAG":latest \
+			--progress=plain \
+			--build-arg="JOBS=$JOBS" \
+			-f "$DIR"/"$NAME"/Dockerfile \
+			--load \
+			2>&1 | tee -a "$LOG"
+	else
+		time DOCKER_BUILDKIT=1 BUILDKIT_PROGRESS=plain docker buildx build \
+			"$DIR" \
+			"$@" \
+			-t "$TAG":latest \
+			--progress=plain \
+			--build-arg="JOBS=$JOBS" \
+			-f "$DIR"/"$NAME"/Dockerfile \
+			--output=type=oci,tar=false,dest="${OCI}" \
+			--output=type=oci,tar=true,dest="${OCI}.tar" \
+			2>&1 | tee -a "$LOG"
+		time docker load -i "${OCI}.tar" 2>&1 | tee -a "$LOG"
+	fi
 	echo "*" | tee -a "$LOG"
 	echo "* $(date): finished $NAME" | tee -a "$LOG"
 	echo "*" | tee -a "$LOG"
